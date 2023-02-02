e75131c12cdd9ff752147fc83c4c63212ce00fe2
antonio <espo-tony@hotmail.it>
2023-02-02 20:56:20 +0100

    Removed module 'website_snippet_dynamic_follow' from addons-generic, as it is now merged in OCA; Replaced all reference to that module within package files with the new reference from OCA; In module nwb_install, removed old module name from dependencies and added the new one;

--- nwb_install/__manifest__.py
+++ nwb_install/__manifest__.py
@@ -155,7 +155,6 @@
         'membership_hr_recruitment',
         'membership_section',
         'web_editor_fontawesome',
-        'website_dynamic_follow',
         'website_event_openstreetmap',
         'website_event_share_filter_option',
         'website_hide_navbar_technical',
@@ -188,6 +187,7 @@
         'website_share_telegram',
         'website_share_tumblr',
         'website_share_wordpress',
+        'website_snippet_dynamic_link',
         'website_two_steps_share_technical',
 
         # 3rd-Party
