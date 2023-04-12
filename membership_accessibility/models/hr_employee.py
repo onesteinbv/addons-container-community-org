dbd77916ba8c2c4f25f9c3780d56397b7c34c206
anjeel.haria <anjeel.haria@brainvire.com>
2023-04-12 20:27:43 +0530

    Updated code to apply groups only after employee is created and updated submoules

--- membership_accessibility/models/hr_employee.py
+++ membership_accessibility/models/hr_employee.py
@@ -1,10 +1,10 @@
 from odoo import _, api, fields, models
 
-class HRApplicant(models.Model):
-    _inherit = 'hr.applicant'
+class HREmployee(models.Model):
+    _inherit = 'hr.employee'
 
     def get_member_groups(self):
-        groups = super(HRApplicant, self).get_member_groups()
+        groups = super(HREmployee, self).get_member_groups()
         groups.append(self.env.ref('sales_team.group_sale_salesman').id)
         groups.append(self.env.ref('project.group_project_user').id)
         groups.append(self.env.ref('hr_timesheet.group_hr_timesheet_user').id)
