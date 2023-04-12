9bd65753102c85528a21b04f26d0d48ffdcba986
anjeel.haria <anjeel.haria@brainvire.com>
2023-04-12 14:40:56 +0530

    Added membership_accessibility module and updated submodules

---
+++ membership_accessibility/models/hr_applicant.py
@@ -0,0 +1,11 @@
+from odoo import _, api, fields, models
+
+class HRApplicant(models.Model):
+    _inherit = 'hr.applicant'
+
+    def get_member_groups(self):
+        groups = super(HRApplicant, self).get_member_groups()
+        groups.append(self.env.ref('sales_team.group_sale_salesman').id)
+        groups.append(self.env.ref('project.group_project_user').id)
+        groups.append(self.env.ref('hr_timesheet.group_hr_timesheet_user').id)
+        return groups
